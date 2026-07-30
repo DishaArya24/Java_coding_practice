e32837720a6494931a304ef2b0dad243c59c343d
Disha <dishaarya389@gmail.com>
2026-07-30 20:27:57 +0530

    sqrt(x)

---
+++ src/Searching_sortiing/sqrt.java
@@ -0,0 +1,49 @@
+package Searching_sortiing;
+
+public class sqrt {
+	
+
+	    public int mySqrt(int x) {
+
+	        if (x == 0 || x == 1) {
+	            return x;
+	        }
+
+	        int left = 1;
+	        int right = x;
+	        int answer = 0;
+
+	        while (left <= right) {
+
+	            int mid = left + (right - left) / 2;
+
+	            long square = (long) mid * mid;
+
+	            if (square == x) {
+	                return mid;
+	            } 
+	            else if (square < x) {
+	                answer = mid;
+	                left = mid + 1;
+	            } 
+	            else {
+	                right = mid - 1;
+	            }
+	        }
+
+	        return answer;
+	    }
+
+	    public static void main(String[] args) {
+
+	        sqrt obj = new sqrt();
+
+	        int x = 8;
+
+	        int result = obj.mySqrt(x);
+
+	        System.out.println("Square root of " + x + " = " + result);
+	    }
+	}
+
+
