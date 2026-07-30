941331b9a33f74d48e90e6e708a4ffc6cad04943
Disha <dishaarya389@gmail.com>
2026-07-30 20:17:53 +0530

    inert positions

---
+++ src/Searching_sortiing/Search_insert_positionn.java
@@ -0,0 +1,35 @@
+package Searching_sortiing;
+
+public class Search_insert_positionn {
+
+    public int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+
+        Search_insert_positionn obj = new Search_insert_positionn();
+
+        int[] nums = {1,3,5,6};
+        int target = 5;
+
+        System.out.println(obj.searchInsert(nums, target));
+    }
+}
