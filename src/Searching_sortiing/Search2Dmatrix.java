34e0224dab26bef51d821953a15f405653ea6986
Disha <dishaarya389@gmail.com>
2026-07-30 20:40:30 +0530

    2D matrix

---
+++ src/Searching_sortiing/Search2Dmatrix.java
@@ -0,0 +1,60 @@
+package Searching_sortiing;
+
+public class Search2Dmatrix {
+	/*
+	 * You are given an m x n integer matrix matrix with the following two properties:
+
+Each row is sorted in non-decreasing order.
+The first integer of each row is greater than the last integer of the previous row.
+Given an integer target, return true if target is in matrix or false otherwise.
+
+You must write a solution in O(log(m * n)) time complexity.
+	 */
+	
+	 public boolean searchMatrix(int[][] matrix, int target) {
+
+	        int rows = matrix.length;
+	        int cols = matrix[0].length;
+
+	        int left = 0;
+	        int right = rows * cols - 1;
+
+	        while (left <= right) {
+
+	            int mid = left + (right - left) / 2;
+
+	            int row = mid / cols;
+	            int col = mid % cols;
+
+	            if (matrix[row][col] == target) {
+	                return true;
+	            }
+	            else if (matrix[row][col] < target) {
+	                left = mid + 1;
+	            }
+	            else {
+	                right = mid - 1;
+	            }
+	        }
+
+	        return false;
+	    }
+
+	    public static void main(String[] args) {
+
+	        Search2Dmatrix obj = new Search2Dmatrix();
+
+	        int[][] matrix = {
+	                {1,3,5,7},
+	                {10,11,16,20},
+	                {23,30,34,60}
+	        };
+
+	        int target = 3;
+
+	        boolean ans = obj.searchMatrix(matrix, target);
+
+	        System.out.println(ans);
+	    }
+
+}
