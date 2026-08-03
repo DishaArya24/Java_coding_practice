565e9c7e1895f1f2571851939e1f4692d0b80d33
Disha <dishaarya389@gmail.com>
2026-08-03 21:17:09 +0530

    linear search

---
+++ src/Searching_sortiing/linearsearch.java
@@ -0,0 +1,22 @@
+package Searching_sortiing;
+
+public class linearsearch {
+	// Linear Search
+	
+	    public static void main(String[] args) {
+	        int[] arr = {10, 20, 30, 40, 50};
+	        int key = 30;
+	        boolean found = false;
+
+	        for (int num : arr) {
+	            if (num == key) {
+	                found = true;
+	                break;
+	            }
+	        }
+
+	        System.out.println(found ? "Found" : "Not Found");
+	    }
+	}
+
+
