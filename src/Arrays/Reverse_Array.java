9fb6fd1ca836111e3b92f4948df3e6b6f3c9e218
Disha <dishaarya389@gmail.com>
2026-08-05 21:37:20 +0530

    rev arr

--- src/Arrays/Reverse_Array.java
+++ src/Arrays/Reverse_Array.java
@@ -9,7 +9,7 @@ public class Reverse_Array {
 		int n = sc.nextInt();
 		System.out.println("Enter the Elements");
 		
-		int[]arr = new int[n];
+		/*int[]arr = new int[n];
 		for(int i=0;i<n;i++) {
 			arr[i] = sc.nextInt();
 		}
@@ -23,13 +23,29 @@ public class Reverse_Array {
 			
 			start++;
 			end--;
+		}*/
+		
+		int [] arr = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int start = 0;
+		int end = n-1;
+		while(start<end) {
+			int temp = arr[start];
+			arr[start]=arr[end];
+			arr[end]=temp;
+			start++;
+			end--;
 		}
 		
 		System.out.println("Reversed Array: ");
-		
 		for(int i=0;i<n;i++) {
-			System.out.println (arr[i]+" ");
+			System.out.println(arr[i]+"");
 		}
+		/*for(int i=0;i<n;i++) {
+			System.out.println (arr[i]+" ");
+		}*/
 		sc.close();
 	}
 
